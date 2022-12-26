bd71e44e79cad4f2188d354944a5cf1e163b6764
MateusSartorio <mateussartorio39@gmail.com>
2022-12-26 20:28:31 -0300

    a little bit more of progress made

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub struct Config {
+    query: String,
+    file_path: String,
+}
+
+pub fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::env;
+use std::fs;
+
+mod lib;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    dbg!(args); 
+    let config = lib::parse_config(&args);
+
 
 }
