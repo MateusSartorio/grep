c868dee005bb192cddb626b1bcf9ec75da6c97ef
MateusSartorio <mateussartorio39@gmail.com>
2022-12-27 18:22:42 -0300

    application completed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::env;
-use std::fs;
+use std::process;
 
-mod lib;
+use grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = lib::parse_config(&args);
-
-
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    
+    if let Err(e) = grep::run(config) {
+        eprintln!("Aplication error: {}", e);
+        process::exit(1);
+    }
 }
